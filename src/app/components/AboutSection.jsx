1a128842ff9349a109d96c9f75eaed08e09093f6
Yash Goyal <yashgoyalambk@gmail.com>
2025-04-10 03:20:58 +0530

    removed img from mobile
    Signed-off-by: Yash Goyal <yashgoyalambk@gmail.com>

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -14,7 +14,6 @@ const TAB_DATA = [
         <li>Express.Js</li>
         <li>Python</li>
         <li>Django</li>
-
       </ul>
     ),
   },
@@ -48,7 +47,7 @@ and I don't mind stepping outside my comfort zone when needed.`;
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={500} height={500} />
+        <Image src="/images/about-image.png" width={500} height={500} className="sm:block hidden" />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
